9ed4299f27d6d1edccdd0f5350d70ef93dfa79c8
mani3354488 <mani3354488@gmail.com>
2021-05-16 20:07:23 +0530

    remove action type

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import {useSelector} from "react-redux"
 
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
-    // const {id, title} = products;
-
-    return (
-        <div className="four column wide">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image"></div>
-                    <div className="content">
-                        {/* <div className="header">{title}</div> */}
+    const renderList = products.map((product) => {
+        const {id, title, category, image, price} = product;
+        return (
+            <div className="four wide column" key={id}>
+            <Link to={`/product/${id}`}>
+                <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image} alt={title} />
+                        </div>
+                        <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">${price}</div>
+                            <div className="meta">{category}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
+        );
+    })
+    return (
+        <>
+            {renderList}
+        </>
     )
 }
 export default ProductComponent;
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -11,4 +11,15 @@ export const productReducer = (state = initialState, {type, payload}) => {
         default:
             return state;
     }
-}
+};
+
+export const selectedProductReducer = (state = {}, {type, payload}) => {
+    switch (type) {
+        case actionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload};
+        case actionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
+        default: 
+            return state;
+    }
+};
